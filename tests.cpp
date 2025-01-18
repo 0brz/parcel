de54565cb8c3dcf46c40bce66e6e0a6e0a5d9ea0
iceiceice <84047808+IndianBleach@users.noreply.github.com>
2025-01-18 18:42:32 +0300

    get logic

--- tests.cpp
+++ tests.cpp
@@ -83,4 +83,48 @@
             else  lx.cursor_move(1);
         }
     }
+
+    ------------------------------------
+    void get_literals() {
+        //string litrs = "tagval: tag: \"apple\" val: \"good\" volume: 123.12478"; // 
+        string litrs = "tagval: tag: \"apple\" val: \"good\" volume: 123.12478"; // 
+        lexer lx(litrs);
+        string cur;
+
+        // we should parse tags and get next format 'word in brackets | float'
+
+        while(lx.can_read()) {
+            char delim;
+            // find tag -> tag=word + :
+            // lx.get_info(cout);
+            short word_len = 0;
+            if ((word_len = lx.next_id(cur)) != lx.npos &&
+                lx.next_symbol(delim) != lx.npos) { // or use next_like with ':'
+                if (delim == ':') {
+                    // this is a tag.
+                    // 
+                    cur += delim;
+                    printf("[tag] %s\n", cur.c_str());
+                }
+                else {
+                    lx.cursor_move(-1);
+                    lx.cursor_move(-word_len);
+                    //lx.
+                }
+            }
+            
+            // this is not a our rule.
+            // maybe this is a literal
+                
+            // word litr
+            if (lx.next_like_rounded(cur, "\"",  "\"", "") != lx.npos) {
+                printf("[litr.word] '%s'\n", cur.c_str());
+            }
+            // float litr
+            else if (lx.next_float(cur) != lx.npos) {
+                printf("[litr.float] '%s'\n", cur.c_str());
+            }
+            else  lx.cursor_move(1);
+        }
+    }
 */
